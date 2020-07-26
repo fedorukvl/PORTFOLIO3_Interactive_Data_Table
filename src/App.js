2a891ae3c186cc05c2d5be130d327a013638c089
Vasiliy <fedorukvl27@gmail.com>
2020-07-26 17:43:37 +0300

    fourth version

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import _ from 'lodash';
 import Loader from './Loader/Loader.js'
 import Table from './Table/Table.js'
 import RowData from './RowData/RowData.js'
+import DataSize from './DataSize/DataSize.js'
 
 class App extends Component {
 
 	state = {
-		dataLoading: true,
+		isDataSizeSelected: false,
+		isDataLoading: false,
 		data: [],
 		sort: 'asc',
 		sortField: 'id',
@@ -15,18 +17,18 @@ class App extends Component {
 	}
 
 	async componentDidMount() {
+		this.setState({isDataLoading:true})
 		const response = await fetch('http://www.filltext.com/?rows=32&id=%7Bnumber%7C1000%7D&firstName=%7BfirstName%7D&lastName=%7BlastName%7D&email=%7Bemail%7D&phone=%7Bphone%7C(xxx)xxx-xx-xx%7D&address=%7BaddressObject%7D&description=%7Blorem%7C32%7D');
 		const data = await response.json();
 		this.setState({
-			dataLoading: false,
+			isDataLoading: false,
 			data: _.orderBy(data, this.state.sortField, this.state.sort)
 		})
 	}
 
 	doSort = (sortField) => {
-		const cloneData=this.state.data.concat();
 		const sortType = this.state.sort === 'asc' ? 'desc' : 'asc';
-		const orderedData = _.orderBy(cloneData,sortField,sortType);
+		const orderedData = _.orderBy(this.state.data,sortField,sortType);
 		this.setState({
 			data: orderedData,
 			sort: sortType,
@@ -38,12 +40,16 @@ class App extends Component {
     	this.setState({row})
   	)
 
+  	selectDataSize = url =>{
+  		console.log(url);
+  	}
+
 
 	render() {
 	  return (
 	    <div className="container">
 	    	{
-	    		this.state.dataLoading
+	    		this.state.isDataLoading
 	    			? <Loader/>
 	    			: <Table 
 	    				data={this.state.data} 
@@ -52,6 +58,9 @@ class App extends Component {
         				sortField={this.state.sortField}
         				onRowSelect={this.onRowSelect}
         				/>
+	    	}
+	    	{
+	    		<DataSize onSelect={this.selectDataSize}/>
 	    	}
 	    	{
 	    		this.state.row
