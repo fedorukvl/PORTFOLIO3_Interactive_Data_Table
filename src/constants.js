82c770c901393fd531285abb91fa132268d0493f
Vasiliy <fedorukvl27@gmail.com>
2020-08-24 21:10:08 +0300

    add smallUrl and bigUrl

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,2 @@
-export const smallUrl = "http://www.filltext.com/?rows=32&id={number|1000}&firstName={firstName}&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}" ;
-export const bigUrl = 'http://www.filltext.com/?rows=1000&id={number|1000}&firstName={firstName}&delay=3&lastName={lastName}&email={email}&phone={phone|(xxx)xxx-xx-xx}&address={addressObject}&description={lorem|32}';
+export const smallUrl = "https://pizza-dbb.herokuapp.com/smallUrl";
+export const bigUrl = "https://pizza-dbb.herokuapp.com/bigUrl";
